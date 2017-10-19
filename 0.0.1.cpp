474112069b5294403c26d25fc3be8f63104578db
Vladimir Alefirov <31669425+Alifa00@users.noreply.github.com>
2017-10-19 22:24:25 +0300

    Update 0.0.1.cpp

--- 0.0.1.cpp
+++ 0.0.1.cpp
@@ -1,109 +1,178 @@
 #include <iostream>
 using namespace std;
-int bmp[4][4];
-void initmas(int u[4]){
-    for(int i=0;i<4;i++){u[i]=0;}
-}
-void init(){
-    bmp[1][0]=4;
-    bmp[1][1]=2;
-    bmp[1][2]=2;
-    bmp[1][3]=2;
-    bmp[2][3]=4;
-    bmp[0][3]=2;
-    bmp[2][2]=4;
-}
-bool up(int uper[4][4]){
-    bool any=true;
-    
-    for(int j=0;j<4;j++){int y[4];initmas(y);int n=0;
-        for(int i=0;i<4;i++){if(uper[j][i]!=0){y[n]=uper[j][i];n++;}}
-        
-        for(int z=0;z<4;z++){if((y[z]==y[z+1])&&(y[z]!=0)){y[z]+=y[z+1];y[z+1]=0;}}
-        
-        for(int w=0;w<3;w++){if(y[w]==0){y[w]=y[w+1];y[w+1]=0;}}
-        
-        for(int i=0;i<4;i++){uper[j][i]=y[i];};
+int map[4][4];
+void initmas(int u[4]) {
+    for (int i = 0; i < 4; i++) {
+        u[i] = 0;
     }
+}
+void init() {
+    map[1][0] = 4;
+    map[1][1] = 2;
+    map[1][2] = 0;
+    map[1][3] = 2;
+    map[2][3] = 4;
+    map[0][3] = 2;
+    map[2][2] = 4;
+}
+bool up(int uper[4][4]) {
+    bool any = true;
     
+    for (int j = 0; j < 4; j++) {
+        int y[4];
+        initmas(y);
+        int n = 0;
+        for (int i = 0; i < 4; i++) {
+            if (uper[j][i] != 0) {
+                y[n] = uper[j][i];
+                n++;
+            }
+        }
+        for (int z = 0; z < 4; z++) {
+            if ((y[z] == y[z + 1]) && (y[z] != 0)) {
+                y[z] += y[z + 1];
+                y[z + 1] = 0;
+            }
+        }
+        for (int w = 0; w < 3; w++) {
+            if (y[w] == 0) {
+                y[w] = y[w + 1];
+                y[w + 1] = 0;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            uper[j][i] = y[i];
+        };
+    }
     return any;
 }
-bool down(int uper[4][4]){
-    bool any=true;
+bool down(int uper[4][4]) {
+    bool any = true;
     
-    for(int j=0;j<4;j++){int y[4];initmas(y);int n=3;
-        for(int i=3;i>-1;i--){if(uper[j][i]!=0){y[n]=uper[j][i];n--;}}
-        
-        for(int z=3;z>0;z--){if((y[z]==y[z-1])&&(y[z]!=0)){y[z]+=y[z-1];y[z-1]=0;}}
-        
-        for(int w=3;w>0;w--){if(y[w]==0){y[w]=y[w-1];y[w-1]=0;}}
-        
-        for(int i=0;i<4;i++){uper[j][i]=y[i];};
+    for (int j = 0; j < 4; j++) {
+        int y[4];
+        initmas(y);
+        int n = 3;
+        for (int i = 3; i > -1; i--) {
+            if (uper[j][i] != 0) {
+                y[n] = uper[j][i];
+                n--;
+            }
+        }
+        for (int z = 3; z > 0; z--) {
+            if ((y[z] == y[z - 1]) && (y[z] != 0)) {
+                y[z] += y[z - 1];
+                y[z - 1] = 0;
+            }
+        }
+        for (int w = 3; w > 0; w--) {
+            if (y[w] == 0) {
+                y[w] = y[w - 1];
+                y[w - 1] = 0;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            uper[j][i] = y[i];
+        };
     }
-    
     return any;
 }
-bool left(int uper[4][4]){
-    bool any=true;
+bool left(int uper[4][4]) {
+    bool any = true;
     
-    for(int j=0;j<4;j++){int y[4];initmas(y);int n=0;
-        for(int i=0;i<4;i++){if(uper[i][j]!=0){y[n]=uper[i][j];n++;}}
-        
-        for(int z=0;z<4;z++){if((y[z]==y[z+1])&&(y[z]!=0)){y[z]+=y[z+1];y[z+1]=0;}}
-        
-        for(int w=0;w<3;w++){if(y[w]==0){y[w]=y[w+1];y[w+1]=0;}}
-        
-        for(int i=0;i<4;i++){uper[i][j]=y[i];};
+    for (int j = 0; j < 4; j++) {
+        int y[4];
+        initmas(y);
+        int n = 0;
+        for (int i = 0; i < 4; i++) {
+            if (uper[i][j] != 0) {
+                y[n] = uper[i][j];
+                n++;
+            }
+        }
+        for (int z = 0; z < 4; z++) {
+            if ((y[z] == y[z + 1]) && (y[z] != 0)) {
+                y[z] += y[z + 1];
+                y[z + 1] = 0;
+            }
+        }
+        for (int w = 0; w < 3; w++) {
+            if (y[w] == 0) {
+                y[w] = y[w + 1];
+                y[w + 1] = 0;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            uper[i][j] = y[i];
+        };
     }
-    
-    
     return any;
 }
-bool right(int uper[4][4]){
-    bool any=true;
-    for(int j=0;j<4;j++){int y[4];initmas(y);int n=3;
-        for(int i=3;i>-1;i--){if(uper[i][j]!=0){y[n]=uper[i][j];n--;}}
-        
-        for(int z=3;z>0;z--){if((y[z]==y[z-1])&&(y[z]!=0)){y[z]+=y[z-1];y[z-1]=0;}}
-        
-        for(int w=3;w>0;w--){if(y[w]==0){y[w]=y[w-1];y[w-1]=0;}}
-        
-        for(int i=0;i<4;i++){uper[i][j]=y[i];};
+bool right(int uper[4][4]) {
+    bool any = true;
+    for (int j = 0; j < 4; j++) {
+        int y[4];
+        initmas(y);
+        int n = 3;
+        for (int i = 3; i > -1; i--) {
+            if (uper[i][j] != 0) {
+                y[n] = uper[i][j];
+                n--;
+            }
+        }
+        for (int z = 3; z > 0; z--) {
+            if ((y[z] == y[z - 1]) && (y[z] != 0)) {
+                y[z] += y[z - 1];
+                y[z - 1] = 0;
+            }
+        }
+        for (int w = 3; w > 0; w--) {
+            if (y[w] == 0) {
+                y[w] = y[w - 1];
+                y[w - 1] = 0;
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            uper[i][j] = y[i];
+        };
     }
-    
-    
     return any;
 }
-void bmpout(){
-    for(int j=0;j<4;j++){
-        for(int i=0;i<4;i++){
-            if(bmp[i][j]==0){cout<<"*"<<" ";continue;}cout<<bmp[i][j]<<" ";
-        } cout<<"\n";
+void mapout() {
+    for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < 4; i++) {
+            if (map[i][j] == 0) {
+                cout << "*" << " ";
+                continue;
+            }
+            cout << map[i][j] << " ";
+        }
+        cout << "\n";
     }
-    cout<<"\n";
+    cout << "\n";
 }
-int main() {char com=' ';
+int main() {
+    char com = ' ';
     init();
     
-    while(com!='q'){
+    while (com != 'q') {
         
-        switch(com){
+        switch (com) {
             case 'j':
-                down(bmp);
+                down(map);
                 break;
             case 'k':
-                up(bmp);
+                up(map);
                 break;
             case 'h':
-                left(bmp);
+                left(map);
                 break;
             case 'l':
-                right(bmp);
+                right(map);
                 break;
-                
-        }
-        bmpout();
-        cin>>com;
+            }
+        mapout();
+        cin >> com;
     }
     return 0;
 }
